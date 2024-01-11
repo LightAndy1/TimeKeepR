bc116ea11113d6fa56b69fb34688f5a69b06dcd8
Light Andy <andy.197197@yahoo.com>
2024-01-11 15:24:49 +0200

    feat(activity): new discord status (starting, started, command)

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,14 +1,24 @@
-require("colors");
+const { ActivityType } = require("discord.js");
 const mongoose = require("mongoose");
 const mongoURL = process.env.MONGO_URL;
+require("colors");
 
 module.exports = async (client) => {
+  await client.user.setActivity("starting...", { type: ActivityType.Playing });
   console.log(`[INFO] ${client.user.username} is online!`.blue);
 
   if (!mongoURL) return;
   mongoose.set("strictQuery", true);
   if (await mongoose.connect(mongoURL)) {
     console.log(`[INFO] Connected to the database!`.green);
+
+    await client.user.setActivity("started", { type: ActivityType.Playing });
+
+    setTimeout(async () => {
+      await client.user.setActivity("/shifts start", {
+        type: ActivityType.Playing,
+      });
+    }, 10_000);
   } else {
     console.log("[ERROR] Failed to connect to the database!".red);
   }
